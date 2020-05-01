40f75ad02374573e7b3ab2f98ad83abf241f564c
Gx3dong <57016139+Gx3dong@users.noreply.github.com>
2020-05-01 22:35:30 +0800

    Update xjpj.js

--- JS/xjpj.js
+++ JS/xjpj.js
@@ -15,7 +15,7 @@ if ($request.url.indexOf("/ucp/index") != -1){
   obj.data.uinfo.minivod_play_daily_remainders = "666";
   obj.data.uinfo.minivod_down_daily_remainders = "666";
   obj.data.uinfo["next_upgrade_need"] = "0";
-  obj.data.user.isvip = 1;
+  obj.data.user.isvip = "1";
   obj.data.user.gicon = "V5";
   obj.data.user.gid = "5";
   obj.data.user.islimitv3 = "5";
@@ -29,10 +29,6 @@ if ($request.url.indexOf("/getGlobalData") != -1){
   obj.data.app_launch_times_adshow = "0";
   obj.data.adgroups = "";
   obj.data.iOS_adgroups = "";
-}
-if ($request.url.indexOf("/vod/show/") != -1){
-   obj.data.vodrow.isvip = "0";
-   obj.data.vodrow.islimitv3 = "0";
 }
 if ($request.url.indexOf("/reqplay/") != -1){
   obj.retcode = "0";
