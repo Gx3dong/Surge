0e3085c2f431028f6ee73e57a960f4673e600dd8
Gx3dong <57016139+Gx3dong@users.noreply.github.com>
2020-05-02 06:27:08 +0800

    Add files via upload

---
+++ JS/plpl.js
@@ -0,0 +1,15 @@
+/*
+#！name=啪哩啪哩unlock vip
+
+[Script]
+http-response https:\/\/.\*.gao1gps\.cn\/v1\/user\/info requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/plpl.js
+
+[MITM]
+hostname = *.gao1gps.cn
+*/
+
+var obj = JSON.parse($response.body);
+obj.response.level = 1;
+obj.response.expiry = 1777594638;
+
+$done({body: JSON.stringify(obj)});
