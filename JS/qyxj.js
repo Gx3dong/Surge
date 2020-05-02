c635477489b166451b2363c11398268a8d6a93d8
Gx3dong <57016139+Gx3dong@users.noreply.github.com>
2020-05-02 15:56:43 +0800

    Add files via upload

---
+++ JS/qyxj.js
@@ -0,0 +1,19 @@
+/*
+#!name=轻言相机unlock vip
+#!desc=轻言相机破解订阅
+
+[Script]
+http-response https:\/\/.*\.faceu\.mobi\/commerce\/v1\/subscription\/user_info requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/qyxj.js
+
+[MITM]
+hostname =*.faceu.mobi
+
+*/
+
+var obj = JSON.parse($response.body);
+
+obj.data.flag = ture;
+obj.data.start_time = 1588318697;
+obj.data.end_time = 1777621097;
+
+$done({body: JSON.stringify(obj)});
