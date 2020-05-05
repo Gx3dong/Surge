04ec4f7ad909c83a896e3e4894397fa4d8570fa9
Gx3dong <xd_891102@163.com>
2020-05-06 02:29:42 +0800

    测试上传更新

--- JS/plpl.js
+++ JS/plpl.js
@@ -1,5 +1,6 @@
 /*
 #！name=啪哩啪哩unlock vip
+#!desc=测试修改 
 
 [Script]
 http-response https:\/\/.*\.(mister-zhou&gao1gps)\.cn\/v1\/user\/info requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/plpl.js
