f2de10236b7f3caa38f3b4c369d64108c9aa4baa
Gx3dong <Gx3dong@qq.com>
2020-05-21 21:51:16 +0800

    up

---
+++ JS/xxys.js
@@ -0,0 +1,41 @@
+/*
+XiaoXiaoYingShi unlock Vip
+QX:（地址加这个）
+https:\/\/.*\.xxjjappss\.com\/(vod\/reqplay\/|ucp/index|getGlobalData) url script-response-body https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/xxys.js
+Surge:
+http-response https:\/\/.*\.xxjjappss\.com\/(vod\/reqplay\/|ucp/index|getGlobalData) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/xxys.js
+这是通过NobyDa大佬的基础上修改好的，小小影视的脚本
+
+mitm加下面的
+[MITM]
+hostname= *.xxjjappss.com
+
+小小影视下载地址：http://IDINEK.xiaoxiaoys1.com/?inviteCode=IDINEK
+*/
+
+const path1 = "/ucp/index";
+const path2 = "/vod/reqplay/";
+const ad = 'getGlobalData';
+let obj = JSON.parse($response.body);
+
+if ($request.url.indexOf(path1) != -1){
+	obj.data.uinfo["down_daily_remainders"] = "666";
+	obj.data.uinfo["play_daily_remainders"] = "666";
+	obj.data.uinfo["curr_group"] = "5";
+	obj.data.user["isvip"] = "1";
+	obj.data.user["goldcoin"] = "666";
+}
+if ($request.url.indexOf(path2) != -1){
+	obj.retcode = "0";
+	obj.data.lastplayindex = "1";
+	if(obj.data.hasOwnProperty("httpurl_preview")){
+		var playurl = obj.data["httpurl_preview"];
+		obj.data["httpurl"] = playurl;
+	};
+}
+
+if ($request.url.indexOf(ad) != -1) {
+delete obj.data.adrows
+delete obj.data.adgroups
+}
+$done({body: JSON.stringify(obj)});
