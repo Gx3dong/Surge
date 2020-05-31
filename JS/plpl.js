ac1e69ec7a8d189177e51bbef47fcd679e34b979
Gx3dong <gx3dong@qq.com>
2020-05-31 19:29:36 +0800

    up

--- JS/plpl.js
+++ JS/plpl.js
@@ -9,8 +9,8 @@ hostname = *.gao1gps.cn, *.mister-zhou.cn,
 */
 
 var obj = JSON.parse($response.body);
-
+if ($request.url.indexOf("/v1/user/info") != -1){
 obj.response.level = 1;
 obj.response.expiry = 1777594638;
-
+}
 $done({body: JSON.stringify(obj)});
