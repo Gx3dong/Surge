53e88a9bf53cc409c3436d974f86ed27f29bb163
Gx3dong <gx3dong@qq.com>
2020-06-01 11:56:47 +0800

    修复

--- JS/plpl.js
+++ JS/plpl.js
@@ -3,16 +3,16 @@
 #!desc=下载：pali.cc
 
 QuanX：
-https:\/\/.*\.(mister-zhou&gao1gps)\.cn\/v1\/user\/info url script-response-body https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/plpl.js
+https:\/\/.*\.gao1gps\.cn\/v1\/user\/info url script-response-body https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/plpl.js
 
 
 surge4:
 
 [Script]
-http-response https:\/\/.*\.(mister-zhou&gao1gps)\.cn\/v1\/user\/info requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/plpl.js
+http-response https:\/\/.*\.gao1gps\.cn\/v1\/user\/info requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/plpl.js
 
 [MITM]
-hostname = *.gao1gps.cn, *.mister-zhou.cn,
+hostname = *.gao1gps.cn
 */
 
 var obj = JSON.parse($response.body);
