de1d041ff49fee642a83995b47c34a87ebe9aca1
Gx3dong <gx3dong@qq.com>
2020-06-04 22:32:58 +0800

    添加去首页广告

--- JS/plpl.js
+++ JS/plpl.js
@@ -19,5 +19,6 @@ var obj = JSON.parse($response.body);
 if ($request.url.indexOf("/v1/user/info") != -1){
 obj.response.level = 1;
 obj.response.expiry = 1777594638;
+obj.response.show_time = 0;
 }
 $done({body: JSON.stringify(obj)});
