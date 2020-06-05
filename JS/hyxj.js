48e6ab8e599312bebff7ad5fb4590935a75cdbc9
Gx3dong <gx3dong@qq.com>
2020-06-05 08:18:46 +0800

    up

--- JS/hyxj.js
+++ JS/hyxj.js
@@ -22,21 +22,21 @@ if ($request.url.indexOf("/v4/shop/shelves") != -1){
     obj.data.trial = "true";
 }
 if ($request.url.indexOf("/v4/editor/template/recommended") != -1){
-    var masking = document.querySelectorAll(masking);
-    var colorful = document.querySelectorAll(colorful);
-    masking = "true";
-    colorful = "true";
+    var masking = document.querySelectorAll("masking");
+    var colorful = document.querySelectorAll("colorful");
+    masking.value = "true";
+    colorful.value = "true";
 }
 if ($request.url.indexOf("/v4/search/products") != -1){
-    var enable = document.querySelectorAll(enable);
-    var trial = document.querySelectorAll(trial);
-    enable = "true";
-    trial = "true";
+    var enable = document.querySelectorAll("enable");
+    var trial = document.querySelectorAll("trial");
+    enable.value = "true";
+    trial.value = "true";
 }
 if ($request.url.indexOf("/v4/template-square/") != -1){
-    var usageType = document.querySelectorAll(usageType);
-    var ownership = document.querySelectorAll(ownership);
-    usageType = "unlimited";
-    ownership = "free";
+    var usageType = document.querySelectorAll("usageType");
+    var ownership = document.querySelectorAll("ownership");
+    usageType.value = "unlimited";
+    ownership.value = "free";
 }
 $done({body: JSON.stringify(obj)});
