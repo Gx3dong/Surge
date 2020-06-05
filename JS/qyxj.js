1d4839039bbd0a9172605a6059bef0858029e8d2
Gx3dong <gx3dong@qq.com>
2020-06-06 04:57:10 +0800

    up

--- JS/qyxj.js
+++ JS/qyxj.js
@@ -1,6 +1,6 @@
 /*
-#!name=轻言相机unlock vip
-#!desc=轻言相机破解订阅
+轻言相机unlock vip
+
 
 [Script]
 http-response https:\/\/.*\.faceu\.mobi\/commerce\/v1\/subscription\/user_info requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/qyxj.js
