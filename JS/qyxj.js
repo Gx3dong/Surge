8476269a9e8db687b7aecd2e8093e22f63fc4457
Gx3dong <gx3dong@qq.com>
2020-06-07 00:21:23 +0800

    update

--- JS/qyxj.js
+++ JS/qyxj.js
@@ -6,13 +6,12 @@
 http-response https:\/\/.*\.faceu\.mobi\/commerce\/v1\/subscription\/user_info requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/qyxj.js
 
 [MITM]
-hostname =*.faceu.mobi
+hostname = *.faceu.mobi
 
 */
 
 var obj = JSON.parse($response.body);
-
-    obj.data.flag = true;
-    obj.data.start_time = 1588318697;
-    obj.data.end_time = 3043035497;
+    obj["flag"] = true;
+    obj["start_time"] = 1588318697;
+    obj["end_time"] = 3043035497;
 $done({body: JSON.stringify(obj)});
