c6a455c7226bf9e7a2ccf31a677fa5bd8c0ced2c
Gx3dong <gx3dong@qq.com>
2020-06-08 01:38:47 +0800

    update

--- JS/hyxj.js
+++ JS/hyxj.js
@@ -9,5 +9,5 @@ hostname = %APPEND% *.bybutter.com
 */ 
 
 
-body = $response.body.replace(/\"ownership\":.+/g, "\"ownership\":free").replace(/\"enable\":.+/g, "\"enable\":true").replace(/\"memberships\":\[\]/g, "\"memberships\":[{\"endAt\":1650842277,\"id\":\"1\",\"name\":\"普通会员\",\"ownership\":\"free\",\"startAt\":1587425677,\"usageType\":\"unlimited\"}]").replace(/\"trial\":.+/g, "\"trial\":true").replace(/\"colorful\":.+/g, "\"colorful\":true").replace(/\"masking\":.+/g, "\"masking\":true").replace(/"remark":"[^""]+"/g, '\"remark\":\"使用期限：永久"').replace(/\"usageType\":.+/g, "\"usageType\":unlimited")
+body = $response.body.replace(/\"ownership\":.+\/g, "\"ownership\":free").replace(/\"enable\":.+\/g, "\"enable\":true").replace(/\"memberships\":\[\]/g, "\"memberships\":[{\"endAt\":1650842277,\"id\":\"1\",\"name\":\"普通会员\",\"ownership\":\"free\",\"startAt\":1587425677,\"usageType\":\"unlimited\"}]").replace(/\"trial\":.+\/g, "\"trial\":true").replace(/\"colorful\":.+\/g, "\"colorful\":true").replace(/\"masking\":.+\/g, "\"masking\":true").replace(/"remark":"[^""]+"/g, '\"remark\":\"使用期限：永久"').replace(/\"usageType\":.+\/g, "\"usageType\":unlimited")
 $done({body});
