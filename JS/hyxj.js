501b7d8011a12edae0ca9e5ac159061d3eb65aab
Gx3dong <gx3dong@qq.com>
2020-06-08 08:41:33 +0800

    up

--- JS/hyxj.js
+++ JS/hyxj.js
@@ -10,5 +10,32 @@ hostname = %APPEND% *.bybutter.com
 
 
 
-body = $response.body.replace(/\"ownership\":\w+/g, "\"ownership\":free").replace(/\"memberships\":\[\]/g, "\"memberships\":[{\"endAt\":1650842277,\"id\":\"1\",\"name\":\"普通会员\",\"ownership\":\"free\",\"startAt\":1587425677,\"usageType\":\"unlimited\"}]").replace(/\"usageType\":\w+/g, "\"usageType\":unlimited")
-$done({body});
+/*
+黄油相机PRO
+
+[Script]
+http-response https?:\/\/.*\.bybutter\.com\/(v4\/shop\/*|v4\/users\/me|v4\/shop\/shelves|v4\/editor\/template\/recommended|v4\/search\/products|v4\/template-square) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/hyxj.js
+
+[MITM]
+hostname = %APPEND% *.bybutter.com
+*/ 
+
+
+re('"ownership":\\w+@"memberships":\\[\]@"usageType":\\w+','"ownership":free@"memberships":[{\"endAt\":1650842277,\"id\":\"1\",\"name\":\"普通会员\",\"ownership\":\"free\",\"startAt\":1587425677,\"usageType\":\"unlimited\"}]@"usageType":unlimited')
+
+function re() {
+ var body = $response.body;
+ if (arguments[0].includes("@")) {
+  var regs = arguments[0].split("@");
+  var strs = arguments[1].split("@");
+  for (i = 0;i < regs.length;i++) {
+   var reg = new RegExp(regs[i],"g");
+   body = body.replace(reg, strs[i]);
+ }
+}
+ else {
+  var reg = new RegExp(arguments[0],"g");
+  body = body.replace(reg, arguments[1]);
+}
+ $done(body);
+} 
