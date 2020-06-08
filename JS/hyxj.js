85e2a3474e5afcc143438dc44ff42f116f2e419a
Gx3dong <gx3dong@qq.com>
2020-06-08 08:37:08 +0800

    up

--- JS/hyxj.js
+++ JS/hyxj.js
@@ -9,21 +9,6 @@ hostname = %APPEND% *.bybutter.com
 */ 
 
 
-re('"ownership":\\w+@"memberships":\\[\]@"usageType":\\w+','"ownership":free@[{\"endAt\":1650842277,\"id\":\"1\",\"name\":\"普通会员\",\"ownership\":\"free\",\"startAt\":1587425677,\"usageType\":\"unlimited\"}]@"usageType":unlimited')
 
-function re() {
- var body = $response.body;
- if (arguments[0].includes("@")) {
-  var regs = arguments[0].split("@");
-  var strs = arguments[1].split("@");
-  for (i = 0;i < regs.length;i++) {
-   var reg = new RegExp(regs[i],"g");
-   body = body.replace(reg, strs[i]);
- }
-}
- else {
-  var reg = new RegExp(arguments[0],"g");
-  body = body.replace(reg, arguments[1]);
-}
- $done(body);
-} 
+body = $response.body.replace(/\"ownership\":\w+/g, "\"ownership\":free").replace(/\"memberships\":\[\]/g, "\"memberships\":[{\"endAt\":1650842277,\"id\":\"1\",\"name\":\"普通会员\",\"ownership\":\"free\",\"startAt\":1587425677,\"usageType\":\"unlimited\"}]").replace(/\"usageType\":\w+/g, "\"usageType\":unlimited")
+$done({body});
