7f8bb5784afb5fd632ce310a4280ece5299be19c
Gx3dong <gx3dong@qq.com>
2020-06-10 19:31:35 +0800

    up

--- JS/shyjzh.js
+++ JS/shyjzh.js
@@ -10,22 +10,10 @@ QX:先把js文件导入本地
 
 Surge4
 http-response pattern=^https:\/\/api.shayujizhang.com\/account\/detail\/info\/ requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master
-*/
+*/  
 
 
-var obj = JSON.parse($response.body);
-if ($request.url.indexOf("/account/detail/info/*") != -1){
-obj.data.vip = {
-      "status": 1,
-      "finish_date": "2021-07-10 16:28:58",
-      "finish_date_ios": "2021.07.10",
-      "pre_date": "2020-06-10 16:28:58",
-      "days": 329,
-      "last_buy_date": "2020-06-10 16:28:58",
-      "auto_buy": 0,
-      "buy_status": 1,
-      "id": 901828,
-      "pre_status": 1
-    };
-}
-$done({body: JSON.stringify(obj)});
+body = $response.body.replace(/\"vip\":\{\}, '\"vip\":{\"status\":1,\"finish_date\":\"2021-07-10 16:28:58\",\"finish_date_ios\":\"2021.07.10\",\"pre_date\":\"2020-06-10 16:28:58\",\"days\":329,\"last_buy_date\":\"2020-06-10 16:28:58\",\"auto_buy\":0,\"buy_status\":1,\"id\": 901828,\"pre_status\":1
+    }')
+
+$done({body});
