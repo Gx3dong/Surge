ee5aab02f149d5fddb8888fed22a4e26fd763b98
Gx3dong <gx3dong@qq.com>
2020-06-13 01:52:39 +0800

    add

---
+++ JS/CamScanner.js
@@ -0,0 +1,14 @@
+/*
+全能扫描王
+
+https:\/\/api-cs.intsig.net\/purchase\/cs\/query_property
+
+
+hostname = api-cs.intsig.net
+*/
+
+
+
+var obj = JSON.parse($response.body);
+obj={"data":{"psnl_vip_property":{"expiry": "1780675200"}}};
+$done({body: JSON.stringify(obj)});
