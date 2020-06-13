bf5b1e4378debc71c4261fc7a285e5761c83f774
Gx3dong <gx3dong@qq.com>
2020-06-13 22:06:38 +0800

    up

--- JS/xjsp.js
+++ JS/xjsp.js
@@ -1,12 +1,36 @@
 /*
-香蕉
+香蕉视频全功能破解和去广告
 
-[Script]
-http-response https?:\/\/.*\.lagoapps\.com\/(ucp\/index|getGlobalData) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/xjsp.js
+下载地址：下载地址:http://VQD5QD.xjapp390.club?inviteCode=VQD5QD
+
+QQ交流群：1077223830
+
+圈X：
+下载这个js到本地Script下面
+再去复制一下内容到对应的配置
+
+[rewrite_local]
+# > 会员
+https:\/\/.*\.(lagoapps|guoguoapps|pipiapps).com url request-header Cookie:.+ request-header Cookie: xxx_api_auth=3666393732366333343132363833646163623438313064646532653661393135
+# > 去广告
+https?:\/\/.*\.(lagoapps|guoguoapps|pipiapps)\.com\/(ucp\/index|getGlobalData|.+\/reqplay\/|\/comment\/listing\/) url script-response-body xjsp.js
 
 [MITM]
-hostname = *.lagoapps.com
-*/00
+hostname = *.guoguoapps.com, *.pipiapps.com, *.lagoapps.com
+
+Surge4:
+直接复制下面内容新建本地模块
+
+[Header Rewrite]
+https:\/\/.*\.(lagoapps|guoguoapps|pipiapps).com header-replace Cookie xxx_api_auth=3666393732366333343132363833646163623438313064646532653661393135
+
+[Script]
+http-response https?:\/\/.*\.(lagoapps|guoguoapps|pipiapps)\.com\/(ucp\/index|getGlobalData|.+\/reqplay\/|\/comment\/listing\/) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/xjsp.js
+
+[MITM]
+hostname = %APPEND% *.guoguoapps.com, *.pipiapps.com, *.lagoapps.com
+
+*/
 
 var obj = JSON.parse($response.body);
 if ($request.url.indexOf("/ucp/index") != -1){
