db52682ab527d2d455bfc13965e11f8b259b045a
Gx3dong <gx3dong@qq.com>
2020-06-13 08:16:04 +0800

    优化

--- JS/xjsp.js
+++ JS/xjsp.js
@@ -2,7 +2,7 @@
 香蕉
 
 [Script]
-http-response https?:\/\/.*\.lagoapps\.com\/(ucp\/index|getGlobalData|.+\/reqplay\/|\/comment\/listing\/) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/xjsp.js
+http-response https?:\/\/.*\.lagoapps\.com\/(ucp\/index|getGlobalData) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/xjsp.js
 
 [MITM]
 hostname = *.lagoapps.com
@@ -10,27 +10,15 @@ hostname = *.lagoapps.com
 
 var obj = JSON.parse($response.body);
 if ($request.url.indexOf("/ucp/index") != -1){
-  obj.data.uinfo.down_daily_remainders = "666";
-  obj.data.uinfo.play_daily_remainders = "666";
-  obj.data.uinfo.minivod_play_daily_remainders = "666";
-  obj.data.uinfo.minivod_down_daily_remainders = "666";
-  obj.data.uinfo["next_upgrade_need"] = "0";
-  obj.data.user.isvip = 1;
-  obj.data.user.gicon = "V5";
-  obj.data.user.gid = "5";
-  obj.data.user.duetime = "2066-06-06 12:12:12";
+  obj.data.uinfo.down_daily_remainders = "999";
+  obj.data.uinfo.play_daily_remainders = "999";
+  obj.data.uinfo.minivod_play_daily_remainders = "999";
+  obj.data.uinfo.minivod_down_daily_remainders = "999";
+
 }
 if ($request.url.indexOf("/getGlobalData") != -1){
-  obj.data.app_launch_times_adshow = "0";
-  obj.data.adgroups = "";
-  obj.data.iOS_adgroups = "";
+  delete obj.data.adgroups;
+  delete obj.data.iOS_adgroups;
 }
-if ($request.url.indexOf("/comment/listing") != -1){
-  obj.data.rows.isvip = 1;
-  obj.data.rows.gicon = "V5";
-}
-if ($request.url.indexOf("/vod/reqplay/") != -1){
-  obj.retcode = "0";
-  
-}
+
 $done({body: JSON.stringify(obj)});
