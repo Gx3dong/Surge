22f45d60dc3673ae3b693e470830ab9dbe938515
Gx3dong <gx3dong@qq.com>
2020-06-14 12:19:36 +0800

    up

--- JS/zhjshg.js
+++ JS/zhjshg.js
@@ -2,19 +2,18 @@
 指尖时光unlock vip
 
 QuanX：
-https:\/\/integral\.dasyibalang\.com\/User url script-response-body zhjshg.js
+https:\/\/integral\.dasyibalang\.com\/User* url script-response-body zhjshg.js
 
 
 surge4:
 
 [Script]
-http-response https:\/\/integral\.dasyibalang\.com\/User requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/zhjshg.js
+http-response https:\/\/integral\.dasyibalang\.com\/User* requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/zhjshg.js
 
 [MITM]
 hostname = integral.dasyibalang.com
 */
 
 
-let obj = JSON.parse($response.body);
-obj = {"data":{"VIP": 1}};
-$done({body: JSON.stringify(obj)});
+body = $response.body.replace(/\"VIP\": \d/g, "\"VIP\": 1")
+$done({body});
