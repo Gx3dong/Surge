196a200b4520940c6211019c409d71ab9f646d23
Gx3dong <gx3dong@qq.com>
2020-06-15 03:30:21 +0800

    up

--- JS/xxys.js
+++ JS/xxys.js
@@ -34,6 +34,7 @@ if ($request.url.indexOf(path2) != -1){
 if ($request.url.indexOf(ad) != -1) {
 delete obj.data.adrows;
 delete obj.data.adgroups;
+delete obj.data.iOS_adgroups;
 obj.data.app_launch_times_adshow =0;
 }
 $done({body: JSON.stringify(obj)});
