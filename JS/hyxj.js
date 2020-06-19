b53d00c6c75cbd1f67afbd94e268d366b1dbbf3c
Gx3dong <gx3dong@qq.com>
2020-06-20 03:34:04 +0800

    update

--- JS/hyxj.js
+++ JS/hyxj.js
@@ -6,15 +6,15 @@
 
 QX1.0.10：
 [rewrite_local]
-https:\/\/api4\.bybutter\.com\/(v4\/users\/me|v4\/shop\/.*) url script-response-body https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/hyxj.js
+https:\/\/api4\.bybutter\.com\/(v4|v4\/users\/me|v4\/shop\/.*) url script-response-body https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/hyxj.js
 
 surge4:
 [Script]
-http-response https:\/\/api4\.bybutter\.com\/(v4\/users\/me|v4\/shop\/.*) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/hyxj.js
+http-response https:\/\/api4\.bybutter\.com\/(v4|v4\/users\/me|v4\/shop\/.*) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/hyxj.js
 
 [MITM]
 hostname = %APPEND% api4.bybutter.com
 */
 
-body = $response.body.replace(/\"ownership\":\"\w+\"/g, '\"ownership\":"free"').replace(/\"usageType\":\"\w+\"/g, '\"usageType\":"unlimited"').replace(/\"memberships\":\[\]/g, "\"memberships\":[{\"endAt\":1650842277,\"id\":\"1\",\"name\":\"普通会员\",\"ownership\":\"temporary\",\"startAt\":1587425677,\"usageType\":\"unlimited\"}]")
+body = $response.body.replace(/\"ownership\":\"\w+\"/g, '\"ownership\":"free"').replace(/\"usageType\":\"\w+\"/g, '\"usageType\":"unlimited"').replace(/\"memberships\":\[\]/g, "\"memberships\":[{\"endAt\":1650842277,\"id\":\"1\",\"name\":\"普通会员\",\"ownership\":\"temporary\",\"startAt\":1587425677,\"usageType\":\"unlimited\"}]").replace(/false/g, "true")
 $done({body});
