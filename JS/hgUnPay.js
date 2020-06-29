f212f335f51ca8c140dffb6f75c6c4aa00e78d6a
Gx3dong <gx3dong@qq.com>
2020-06-29 23:42:43 +0800

    add

---
+++ JS/hgUnPay.js
@@ -0,0 +1,31 @@
+/*
+APP:黄瓜视频单独破解付费
+
+>Gx3dong制作,免费资源群:1077223830.进群不迷路.
+>木木团队,欢迎关注公众号:十三座州府
+
+下载地址:https://michen.gdyhws.space/jhy7624.html?pkg=cuke0347&accountCode=666&inviteCode=CEN76X
+
+QQ交流群：1077223830
+
+圈X：
+
+[rewrite_local]
+https:\/\/.*\.awk2\.work\/mov\/browse2 url script-response-body https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/hgUnPay.js
+
+[MITM]
+hostname = *.awk2.work
+
+Surge4:
+直接复制下面内容新建本地模块
+
+[Script]
+http-response https:\/\/.*\.awk2\.work\/mov\/browse2 requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/hgUnPay.js
+
+[MITM]
+hostname = *.awk2.work
+
+*/
+
+body = $response.body.replace(/\"hasBuy\":\w+/g, "\"hasBuy\":true").replace(/\"msg\":"[^""]*"/g, '\"msg\":\"1\"')
+$done({body});
