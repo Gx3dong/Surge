3c25756f94f2e928b9894ddf24b4a3c53e98ae6b
Gx3dong <gx3dong@qq.com>
2020-07-01 19:12:01 +0800

    修复真实播放次数

--- JS/xjsp.js
+++ JS/xjsp.js
@@ -16,7 +16,7 @@ QQ交流群：1077223830
 # > 会员
 https:\/\/.*\.(lagoapps|guoguoapps|pipiapps).com url request-header Cookie:.+ request-header Cookie: xxx_api_auth=3666393732366333343132363833646163623438313064646532653661393135
 # > 去广告
-https?:\/\/.*\.(lagoapps|guoguoapps|pipiapps)\.com\/(ucp\/index|getGlobalData|.+\/reqplay\/|\/comment\/listing\/) url script-response-body https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/xjsp.js
+https?:\/\/.*\.(lagoapps|guoguoapps|pipiapps)\.com\/(ucp\/index|getGlobalData|.+\/reqplay\/) url script-response-body https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/xjsp.js
 
 [MITM]
 hostname = *.guoguoapps.com, *.pipiapps.com, *.lagoapps.com
@@ -28,7 +28,7 @@ Surge4:
 https:\/\/.*\.(lagoapps|guoguoapps|pipiapps).com header-replace Cookie xxx_api_auth=3666393732366333343132363833646163623438313064646532653661393135
 
 [Script]
-http-response https?:\/\/.*\.(lagoapps|guoguoapps|pipiapps)\.com\/(ucp\/index|getGlobalData|.+\/reqplay\/|\/comment\/listing\/) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/xjsp.js
+http-response https?:\/\/.*\.(lagoapps|guoguoapps|pipiapps)\.com\/(ucp\/index|getGlobalData|.+\/reqplay\/) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/xjsp.js
 
 [MITM]
 hostname = %APPEND% *.guoguoapps.com, *.pipiapps.com, *.lagoapps.com
@@ -46,6 +46,13 @@ obj.data.user.mobi = "QQ群:1077223830";
 if ($request.url.indexOf("/getGlobalData") != -1){
   delete obj.data.adgroups;
   delete obj.data.iOS_adgroups;
+}
+if ($request.url.indexOf("/reqplay/") != -1){
+  obj.retcode = "0";
+  if(obj.data.hasOwnProperty("httpurl_preview")){
+  var playurl = obj.data["httpurl_preview"];
+  obj.data["httpurl"] = playurl;
+  };
 }
 
 $done({body: JSON.stringify(obj)});
