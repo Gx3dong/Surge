c6d786e6ab8367f2b9d5b4ff41002b75a0d17145
Gx3dong <Gx3dong@qq.com>
2020-11-27 03:20:18 +0800

    更新

--- JS/hgUnPay.js
+++ JS/hgUnPay.js
@@ -11,19 +11,19 @@ QQ交流群：1077223830
 圈X：
 
 [rewrite_local]
-https:\/\/.*\.awk2\.work\/mov\/browse2 url script-response-body https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/hgUnPay.js
+https:\/\/api1000\.gdqeb\.club\/mov\/browse2 url script-response-body https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/hgUnPay.js
 
 [MITM]
-hostname = *.awk2.work
+hostname = api1000.gdqeb.club
 
 Surge4:
 直接复制下面内容新建本地模块
 
 [Script]
-http-response https:\/\/.*\.awk2\.work\/mov\/browse2 requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/hgUnPay.js
+http-response https:\/\/api1000\.gdqeb\.club\/mov\/browse2 requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/hgUnPay.js
 
 [MITM]
-hostname = *.awk2.work
+hostname = api1000.gdqeb.club
 
 */
 
