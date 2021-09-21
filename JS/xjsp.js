2f60e5969a1eabb9bed330a15d0dd5a740350a6a
Gx3dong <gx3dong@qq.com>
2021-09-21 14:40:16 +0800

    更新Cookie

--- JS/xjsp.js
+++ JS/xjsp.js
@@ -14,7 +14,7 @@ QQ交流群：1077223830
 
 [rewrite_local]
 # > 会员
-https:\/\/.*\.*apps.com url request-header Cookie:.+ request-header Cookie: xxx_api_auth=3162386365306537353636306434346135343132323732666230356531653833
+https:\/\/.*\.*apps.com url request-header Cookie:.+ request-header Cookie: xxx_api_auth=6661366530633937373937653461323662393366663038636634356139353330
 # > 去广告
 https?:\/\/.*\.*apps\.com\/(ucp\/index|getGlobalData|.+\/reqplay\/) url script-response-body https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/xjsp.js
 
@@ -25,7 +25,7 @@ Surge4:
 直接复制下面内容新建本地模块
 
 [Header Rewrite]
-https:\/\/.*\.*apps\.com header-replace Cookie xxx_api_auth=3162386365306537353636306434346135343132323732666230356531653833
+https:\/\/.*\.*apps\.com header-replace Cookie xxx_api_auth=6661366530633937373937653461323662393366663038636634356139353330
 
 [Script]
 http-response https?:\/\/.*\.*apps\.com\/(ucp\/index|getGlobalData|.+\/reqplay\/) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/xjsp.js
